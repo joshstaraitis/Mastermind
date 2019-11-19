1dfe0560d8117cfd30c600aa09c768a1349d945e
joshstaraitis <52257689+joshstaraitis@users.noreply.github.com>
2019-11-19 10:54:47 -0500

    Add files via upload

---
+++ Mastermind/Attempt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mastermind
+{
+    public class Attempt
+    {
+         private readonly string AnswerCode;
+
+        public Attempt(string code)
+        {
+            AnswerCode = code;
+        }
+
+        public string Analyze(string attempt)
+        {
+            var builder = new StringBuilder(attempt.Length);
+
+            for (var index = 0; index < attempt.Length; index++)
+            {
+                var digit = attempt[index];
+                var information = ' ';
+
+                if (AnswerCode.Contains(digit))
+                    information = AnswerCode[index] == digit
+                        ? '+'
+                        : '-';
+
+                builder.Append(information);
+            }
+            return builder.ToString();
+        }
+    }
+}
